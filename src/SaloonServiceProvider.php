c9d8f18ff55ce77bd5509dbce9e50dbbd2a616f2
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-01-18 19:10:18 +0000

    wip

--- src/SaloonServiceProvider.php
+++ src/SaloonServiceProvider.php
@@ -2,22 +2,21 @@
 
 namespace Sammyjo20\SaloonLaravel;
 
-use Illuminate\Support\ServiceProvider;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakeRequest;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakeConnector;
+use \Sammyjo20\Saloon\Laravel\SaloonServiceProvider as BaseSaloonServiceProvider;
 
-class SaloonServiceProvider extends ServiceProvider
+class SaloonServiceProvider extends BaseSaloonServiceProvider
 {
     public function boot(): void
     {
+        parent::boot();
+
         if ($this->app->runningInConsole()) {
             $this->registerCommands();
         }
     }
 
-    /**
-     * @return $this
-     */
     protected function registerCommands(): self
     {
         $this->commands([
