af63512bb3ce262e39879f934283187ab959b4e8
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-01-18 16:48:16 +0000

    Added tests

--- src/SaloonServiceProvider.php
+++ src/SaloonServiceProvider.php
@@ -3,15 +3,13 @@
 namespace Sammyjo20\SaloonLaravel;
 
 use Illuminate\Support\ServiceProvider;
-use Sammyjo20\SaloonLaravel\Console\Commands\MakeConnector;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakeRequest;
+use Sammyjo20\SaloonLaravel\Console\Commands\MakeConnector;
 
 class SaloonServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Todo: Publish stubs
-
         if ($this->app->runningInConsole()) {
             $this->registerCommands();
         }
