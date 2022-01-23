dd07c38082415eb5853f8070383660a9d53835a1
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-01-23 12:51:43 +0000

    More assertions

---
+++ tests/Feature/RequestTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Sammyjo20\Saloon\Tests\Resources\Requests\UserRequest;
+use Sammyjo20\Saloon\Tests\Resources\Requests\ErrorRequest;
+
+test('a request can be made successfully', function () {
+    $request = new UserRequest();
+    $response = $request->send();
+
+    $data = $response->json();
+
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(200);
+
+    expect($data)->toEqual([
+        'name' => 'Sammyjo20',
+        'actual_name' => 'Sam',
+        'twitter' => '@carre_sam',
+    ]);
+});
+
+test('a request can handle an exception properly', function () {
+    $request = new ErrorRequest();
+    $response = $request->send();
+
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(500);
+});
