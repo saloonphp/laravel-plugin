61e2901f4f55ae383a641406f78cc0065ff314da
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-01-28 20:39:03 +0000

    Added new commands, upgraded to Saloon v0.5

--- src/SaloonServiceProvider.php
+++ src/SaloonServiceProvider.php
@@ -6,8 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Sammyjo20\SaloonLaravel\Clients\MockClient;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakePlugin;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakeRequest;
-use Sammyjo20\SaloonLaravel\Console\Commands\MakeConnector;
 use Sammyjo20\SaloonLaravel\Console\Commands\MakeResponse;
+use Sammyjo20\SaloonLaravel\Console\Commands\MakeConnector;
 
 class SaloonServiceProvider extends ServiceProvider
 {
