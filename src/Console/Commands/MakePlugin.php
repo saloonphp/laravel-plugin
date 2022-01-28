01dcd083be66546229390cb8cd6d00da5dab4639
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-01-28 20:35:37 +0000

    New commands

---
+++ src/Console/Commands/MakePlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sammyjo20\SaloonLaravel\Console\Commands;
+
+class MakePlugin extends MakeCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'saloon:plugin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Saloon plugin';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Saloon Plugin';
+
+    /**
+     * The namespace to place the file
+     *
+     * @var string
+     */
+    protected $namespace = '\Http\Saloon\Plugins';
+
+    /**
+     * The default stub
+     *
+     * @var string
+     */
+    protected $stub = 'saloon.plugin.stub';
+}
