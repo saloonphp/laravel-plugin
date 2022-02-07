9ff3ab8b53b08fdf1908988ceb355beece77cbae
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-02-07 00:08:01 +0000

    Added assertion methods to facade

--- src/Saloon.php
+++ src/Saloon.php
@@ -39,4 +39,72 @@ class Saloon
     {
         return MockClient::resolve()->startMocking($responses);
     }
+
+    /**
+     * Retrieve the mock client from the container
+     *
+     * @return MockClient
+     */
+    public static function mockClient(): MockClient
+    {
+        return MockClient::resolve();
+    }
+
+    /**
+     * Assert that a given request was sent.
+     *
+     * @param string|callable $value
+     * @return void
+     * @throws \ReflectionException
+     */
+    public static function assertSent(string|callable $value): void
+    {
+        static::mockClient()->assertSent($value);
+    }
+
+    /**
+     * Assert that a given request was not sent.
+     *
+     * @param string|callable $value
+     * @return void
+     * @throws \ReflectionException
+     */
+    public static function assertNotSent(string|callable $value): void
+    {
+        static::mockClient()->assertNotSent($value);
+    }
+
+    /**
+     * Assert JSON data was sent
+     *
+     * @param string $request
+     * @param array $data
+     * @return void
+     * @throws \ReflectionException
+     */
+    public static function assertSentJson(string $request, array $data): void
+    {
+        static::mockClient()->assertSentJson($request, $data);
+    }
+
+    /**
+     * Assert that nothing was sent.
+     *
+     * @return void
+     */
+    public static function assertNothingSent(): void
+    {
+        static::mockClient()->assertNothingSent();
+    }
+
+    /**
+     * Assert a request count has been met.
+     *
+     * @param int $count
+     * @return void
+     */
+    public static function assertSentCount(int $count): void
+    {
+        static::mockClient()->assertSentCount($count);
+    }
 }
