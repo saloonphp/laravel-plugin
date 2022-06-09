9bf4edddea82221d521e5d3a8908c7748ef5b788
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-06-09 20:59:24 +0100

    Custom cast written

---
+++ src/Casts/OAuthAuthenticatorCast.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sammyjo20\SaloonLaravel\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use InvalidArgumentException;
+use Sammyjo20\Saloon\Interfaces\OAuthAuthenticatorInterface;
+
+class OAuthAuthenticatorCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param $model
+     * @param string $key
+     * @param $value
+     * @param array $attributes
+     * @return OAuthAuthenticatorInterface|null
+     */
+    public function get($model, string $key, $value, array $attributes): ?OAuthAuthenticatorInterface
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return unserialize($value, ['allowed_classes' => true]);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param $model
+     * @param string $key
+     * @param $value
+     * @param array $attributes
+     * @return mixed|void
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        if (! $value instanceof OAuthAuthenticatorInterface) {
+            throw new InvalidArgumentException('The given value is not an OAuthAuthenticatorInterface instance.');
+        }
+
+        return serialize($value);
+    }
+}
