2ee55357dafe139e17ed4a17bb6e5b0311172abd
Sammyjo20 <Sammyjo20@users.noreply.github.com>
2023-01-21 12:03:55 +0000

    🪄 Code Style Fixes

--- tests/Feature/SimulatedResponsePayloadTest.php
+++ tests/Feature/SimulatedResponsePayloadTest.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-use Saloon\Http\Faking\MockResponse;
 use Saloon\Laravel\Facades\Saloon;
+use Saloon\Http\Faking\MockResponse;
 use Saloon\Http\Faking\SimulatedResponsePayload;
-use Saloon\Laravel\Tests\Fixtures\Connectors\TestConnector;
 use Saloon\Laravel\Tests\Fixtures\Requests\UserRequest;
+use Saloon\Laravel\Tests\Fixtures\Connectors\TestConnector;
 
 test('if a simulated response payload was provided before mock response it will take priority', function () {
     Saloon::fake([
