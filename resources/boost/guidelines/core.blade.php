7a02b45d3f38cc567f3193f80aa370354a78704a
Thomas van der Westen <thomas@codesmiths.nl>
2026-01-31 20:53:39 +0100

    Fix formatting in SaloonPHP guidelines for consistency.

--- resources/boost/guidelines/core.blade.php
+++ resources/boost/guidelines/core.blade.php
@@ -1,6 +1,6 @@
 ## SaloonPHP
 
-PHP library for building API integrations and SDKs.
-Documentation: https://docs.saloon.dev
-Check `composer.json` for version (v2 or v3). Use `web-search` tool for latest docs before implementing.
-IMPORTANT: Activate saloon-development skill when working with SaloonPHP-related tasks.
+- PHP library for building API integrations and SDKs.
+- Documentation: https://docs.saloon.dev
+- Check `composer.json` for version (v2 or v3). Use `web-search` tool for latest docs before implementing.
+- IMPORTANT: Activate saloon-development skill when working with SaloonPHP-related tasks.
