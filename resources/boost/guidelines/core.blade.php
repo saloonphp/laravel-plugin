c22de3b1ccccc2c6dce3573e9a884fd8fa89ada6
Jess Stopa <jess@stopa.dk>
2026-09-04 22:19:00 +0200

    fix: duplicate php artisan command (#87)

--- resources/boost/guidelines/core.blade.php
+++ resources/boost/guidelines/core.blade.php
@@ -8,5 +8,5 @@
 - Documentation: https://docs.saloon.dev
 - Check `composer.json` for version (v2, v3 or v4). Use `web-search` tool for latest docs before implementing.
 - If the project uses v3, inform the user that v3 is affected by three published security issues and prefer upgrading to v4 before adding new Saloon work, unless the user explicitly needs a v3-compatible change.
-- Always use Artisan commands to generate SaloonPHP classes: `{{ $assist->artisanCommand('saloon:connector') }}`, `{{ $assist->artisanCommand('saloon:request') }}`, `{{ $assist->artisanCommand('saloon:response') }}`, `{{ $assist->artisanCommand('php artisan saloon:plugin') }}`, `{{ $assist->artisanCommand('saloon:auth') }}`.- Documentation: `https://docs.saloon.dev`
+- Always use Artisan commands to generate SaloonPHP classes: `{{ $assist->artisanCommand('saloon:connector') }}`, `{{ $assist->artisanCommand('saloon:request') }}`, `{{ $assist->artisanCommand('saloon:response') }}`, `{{ $assist->artisanCommand('saloon:plugin') }}`, `{{ $assist->artisanCommand('saloon:auth') }}`.- Documentation: `https://docs.saloon.dev`
 - IMPORTANT: Activate saloon-development skill when working with SaloonPHP-related tasks.
